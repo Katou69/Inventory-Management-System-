177c6701a399eae162b4e792f92de9891f971206
Katou69 <u6530226@au.edu>
2026-07-13 23:44:24 +0700

    Changed view back to admin

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,7 +2,7 @@ import { StaffDashboardContent} from "@/components/dashboard/staff";
 import { AdminDashboardContent } from "@/components/dashboard/admin";
 import { ManagerDashboardContent } from "@/components/dashboard/manager";
 
-export const role:string = "manager"; // later from auth/session, currently hardcoded for testing purposes(exporting so that it can be used in other files like inventory/page.tsx and other files)
+export const role:string = "admin"; // later from auth/session, currently hardcoded for testing purposes(exporting so that it can be used in other files like inventory/page.tsx and other files)
 export default function DashboardPage() {
     
 
